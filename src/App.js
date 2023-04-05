cc520cfb7e689f292292a0a8caf661f930abe097
Frank Asamoah <frank.asamoah119@gmail.com>
2023-04-05 23:43:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+// import hero from "./images/pattern-bg.png";
+import hero from "./images/hero.png";
+import arrow from "./images/icon-arrow.svg";
+import Markerposition from "./components/MarkerPosition";
 
 function App() {
+  const [address, setAddress] = useState(null);
+  const [ipAddress, setIpAddress] = useState("");
+  const veryIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+  const veryDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
+
+  useEffect(() => {
+    try {
+      const getInitialData = async () => {
+        const { data } = await axios(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=195.138.42.15`
+        );
+        setAddress(data);
+      };
+
+      getInitialData();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const getTypedData = async () => {
+    const { data } = await axios(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${
+        process.env.REACT_APP_API_KEY
+      }&${
+        veryIpAddress.test(ipAddress)
+          ? `ipAddress=${ipAddress}`
+          : veryDomain.test(ipAddress)
+          ? `domain=${ipAddress}`
+          : ""
+      }`
+    );
+
+    setAddress(data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getTypedData();
+    setIpAddress("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <div className="absolute w-full -z-10">
+          <img src={hero} alt="" className="w-full h-80" />
+        </div>
+
+        <div className="max-w-xl mx-auto p-8">
+          <h1 className="font-bold text-2xl lg:text-3xl text-white pb-8 text-center">
+            IP Address Checker
+          </h1>
+
+          <form
+            onSubmit={handleSubmit}
+            autoComplete="off"
+            className="w-full flex"
+          >
+            <input
+              type="text"
+              name="ipaddress"
+              id="ipaddress"
+              placeholder="Type any ip address or domain"
+              className="w-full py-2 px-4 rounded-l-lg"
+              value={ipAddress}
+              onChange={(e) => setIpAddress(e.target.value)}
+            />
+            <button type="submit" className="bg-black py-2 px-4 rounded-r-lg">
+              <img src={arrow} alt="" />
+            </button>
+          </form>
+        </div>
+
+        {address && (
+          <>
+            <article className="p-8">
+              <div
+                className="bg-white rounded-xl p-8 shadow max-w-6xl mx-auto grid grid-cols-1 gap-5 text-center md:grid-cols-2 lg:grid-cols-4 lg:gap-0 lg:text-left -mb-10 relative lg:-mb-32"
+                style={{
+                  zIndex: 10000,
+                }}
+              >
+                <article className="lg:border-r lg:border-slate-400 p-6">
+                  <h2 className="text-sm uppercase text-slate-600">
+                    IP Address
+                  </h2>
+                  <p className="font-bold text-slate-900 text-2xl">
+                    {address.ip}
+                  </p>
+                </article>
+
+                <article className="lg:border-r lg:border-slate-400 p-6">
+                  <h2 className="text-sm uppercase text-slate-600">Location</h2>
+                  <p className="font-bold text-slate-900 text-2xl">
+                    {address.location.city}, {address.location.region}
+                  </p>
+                </article>
+
+                <article className="lg:border-r lg:border-slate-400 p-6">
+                  <h2 className="text-sm uppercase text-slate-600">Timezone</h2>
+                  <p className="font-bold text-slate-900 text-2xl">
+                    UTC {address.location.timezone}
+                  </p>
+                </article>
+
+                <article className="p-6">
+                  <h2 className="text-sm uppercase text-slate-600">ISP</h2>
+                  <p className="font-bold text-slate-900 text-2xl">
+                    {address.isp}
+                  </p>
+                </article>
+              </div>
+            </article>
+
+            <MapContainer
+              center={[address.location.lat, address.location.lng]}
+              zoom={13}
+              scrollWheelZoom={true}
+              style={{ height: "100vh", width: "100vw" }}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Markerposition address={address} />
+            </MapContainer>
+          </>
+        )}
+      </section>
+    </>
   );
 }
 
